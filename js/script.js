0bdc0101f15b117d0abc986be62a0e234b3b6a5f
Andrew <andrew.narolsky@gmail.com>
2021-09-20 16:07:49 +0300

    fix js

--- js/script.js
+++ js/script.js
@@ -61,88 +61,89 @@
 //             }
 //         }
 //     }
-// }
 //
-// FindVideos.prototype.setupVideo = function($video) {
-//     let $link = $video.querySelector('.video__link');
-//     let $media = $video.querySelector('.video__media');
-//     let $button = $video.querySelector('.video__button');
-//     let id = this.parseMediaURL($media);
-//     $video.addEventListener('click', () => {
-//         let $iframe = this.createIframe(id);
-//         $link.remove();
-//         $button.remove();
-//         $video.appendChild($iframe);
-//     });
-//     $link.removeAttribute('href');
-//     $video.classList.add('video--enabled');
-// }
+//     this.setupVideo = function($video) {
+//         let $link = $video.querySelector('.video__link');
+//         let $media = $video.querySelector('.video__media');
+//         let $button = $video.querySelector('.video__button');
+//         let id = this.parseMediaURL($media);
+//         $video.addEventListener('click', () => {
+//             let $iframe = this.createIframe(id);
+//             $link.remove();
+//             $button.remove();
+//             $video.appendChild($iframe);
+//         });
+//         $link.removeAttribute('href');
+//         $video.classList.add('video--enabled');
+//     }
 //
-// FindVideos.prototype.parseMediaURL = function($media) {
-//     let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
-//     let url = $media.src;
-//     let match = url.match(regexp);
-//     return match[1];
-// }
+//     this.parseMediaURL = function($media) {
+//         let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+//         let url = $media.src;
+//         let match = url.match(regexp);
+//         return match[1];
+//     }
 //
-// FindVideos.prototype.createIframe = function(id) {
-//     let $iframe = document.createElement('iframe');
-//     $iframe.setAttribute('allowfullscreen', '');
-//     $iframe.setAttribute('allow', 'autoplay');
-//     $iframe.setAttribute('src', this.generateURL(id));
-//     $iframe.classList.add('video__media');
-//     return $iframe;
-// }
+//     this.createIframe = function(id) {
+//         let $iframe = document.createElement('iframe');
+//         $iframe.setAttribute('allowfullscreen', '');
+//         $iframe.setAttribute('allow', 'autoplay');
+//         $iframe.setAttribute('src', this.generateURL(id));
+//         $iframe.classList.add('video__media');
+//         return $iframe;
+//     }
 //
-// FindVideos.prototype.generateURL = function(id) {
-//     let query = '?rel=0&showinfo=0&autoplay=1';
-//     return 'https://www.youtube.com/embed/' + id + query;
+//     this.generateURL = function(id) {
+//         let query = '?rel=0&showinfo=0&autoplay=1';
+//         return 'https://www.youtube.com/embed/' + id + query;
+//     }
 // }
 
 const FindVideos = function() {
+    this.init();
+}
 
-    this.init = function() {
-        let $videos = document.querySelectorAll('.view-block-wrap');
-        for (let i in $videos) {
-            if ($videos.hasOwnProperty(i)) {
-                this.setupVideo($videos[i]);
-            }
+FindVideos.prototype.init = function() {
+    let $videos = document.querySelectorAll('.view-block-wrap');
+    for (let i in $videos) {
+        if ($videos.hasOwnProperty(i)) {
+            this.setupVideo($videos[i]);
         }
     }
+}
 
-    this.setupVideo = function($video) {
-        let $link = $video.querySelector('.video__link');
-        let $media = $video.querySelector('.video__media');
-        let $button = $video.querySelector('.video__button');
-        let id = this.parseMediaURL($media);
-        $video.addEventListener('click', () => {
-            let $iframe = this.createIframe(id);
-            $link.remove();
-            $button.remove();
-            $video.appendChild($iframe);
-        });
-        $link.removeAttribute('href');
-        $video.classList.add('video--enabled');
-    }
+FindVideos.prototype.setupVideo = function($video) {
+    let $link = $video.querySelector('.video__link');
+    let $media = $video.querySelector('.video__media');
+    let $button = $video.querySelector('.video__button');
+    let id = this.parseMediaURL($media);
+    $video.addEventListener('click', () => {
+        let $iframe = this.createIframe(id);
+        $link.remove();
+        $button.remove();
+        $video.appendChild($iframe);
+    });
+    $link.removeAttribute('href');
+    $video.classList.add('video--enabled');
+}
 
-    this.parseMediaURL = function($media) {
-        let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
-        let url = $media.src;
-        let match = url.match(regexp);
-        return match[1];
-    }
+FindVideos.prototype.parseMediaURL = function($media) {
+    let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+    let url = $media.src;
+    let match = url.match(regexp);
+    return match[1];
+}
 
-    this.createIframe = function(id) {
-        let $iframe = document.createElement('iframe');
-        $iframe.setAttribute('allowfullscreen', '');
-        $iframe.setAttribute('allow', 'autoplay');
-        $iframe.setAttribute('src', this.generateURL(id));
-        $iframe.classList.add('video__media');
-        return $iframe;
-    }
+FindVideos.prototype.createIframe = function(id) {
+    let $iframe = document.createElement('iframe');
+    $iframe.setAttribute('allowfullscreen', '');
+    $iframe.setAttribute('allow', 'autoplay');
+    $iframe.setAttribute('src', this.generateURL(id));
+    $iframe.classList.add('video__media');
+    return $iframe;
+}
 
-    this.generateURL = function(id) {
-        let query = '?rel=0&showinfo=0&autoplay=1';
-        return 'https://www.youtube.com/embed/' + id + query;
-    }
+FindVideos.prototype.generateURL = function(id) {
+    let query = '?rel=0&showinfo=0&autoplay=1';
+    return 'https://www.youtube.com/embed/' + id + query;
 }
