65af2d417fc4e6285a75179ff1dbcf91ed76eb29
Andrew <andrew.narolsky@gmail.com>
2021-09-21 14:33:59 +0300

    remove arrows functions

--- js/script.js
+++ js/script.js
@@ -1,109 +1,8 @@
-// class FindVideos
-// {
-//     constructor()
-//     {
-//         let $videos = document.querySelectorAll('.view-block-wrap');
-//         for (let i in $videos) {
-//             if ($videos.hasOwnProperty(i)) {
-//                 this.setupVideo($videos[i]);
-//             }
-//         }
-//     }
-//
-//     setupVideo($video)
-//     {
-//         let $link = $video.querySelector('.video__link');
-//         let $media = $video.querySelector('.video__media');
-//         let $button = $video.querySelector('.video__button');
-//         let id = this.parseMediaURL($media);
-//         $video.addEventListener('click', () => {
-//             let $iframe = this.createIframe(id);
-//             $link.remove();
-//             $button.remove();
-//             $video.appendChild($iframe);
-//         });
-//         $link.removeAttribute('href');
-//         $video.classList.add('video--enabled');
-//     }
-//
-//     parseMediaURL($media)
-//     {
-//         let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
-//         let url = $media.src;
-//         let match = url.match(regexp);
-//         return match[1];
-//     }
-//
-//     createIframe(id)
-//     {
-//         let $iframe = document.createElement('iframe');
-//         $iframe.setAttribute('allowfullscreen', '');
-//         $iframe.setAttribute('allow', 'autoplay');
-//         $iframe.setAttribute('src', this.generateURL(id));
-//         $iframe.classList.add('video__media');
-//         return $iframe;
-//     }
-//
-//     generateURL(id)
-//     {
-//         let query = '?rel=0&showinfo=0&autoplay=1';
-//         return 'https://www.youtube.com/embed/' + id + query;
-//     }
-// }
-
-// const FindVideos = function() {
-//
-//     this.init = function() {
-//         let $videos = document.querySelectorAll('.view-block-wrap');
-//         for (let i in $videos) {
-//             if ($videos.hasOwnProperty(i)) {
-//                 this.setupVideo($videos[i]);
-//             }
-//         }
-//     }
-//
-//     this.setupVideo = function($video) {
-//         let $link = $video.querySelector('.video__link');
-//         let $media = $video.querySelector('.video__media');
-//         let $button = $video.querySelector('.video__button');
-//         let id = this.parseMediaURL($media);
-//         $video.addEventListener('click', () => {
-//             let $iframe = this.createIframe(id);
-//             $link.remove();
-//             $button.remove();
-//             $video.appendChild($iframe);
-//         });
-//         $link.removeAttribute('href');
-//         $video.classList.add('video--enabled');
-//     }
-//
-//     this.parseMediaURL = function($media) {
-//         let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
-//         let url = $media.src;
-//         let match = url.match(regexp);
-//         return match[1];
-//     }
-//
-//     this.createIframe = function(id) {
-//         let $iframe = document.createElement('iframe');
-//         $iframe.setAttribute('allowfullscreen', '');
-//         $iframe.setAttribute('allow', 'autoplay');
-//         $iframe.setAttribute('src', this.generateURL(id));
-//         $iframe.classList.add('video__media');
-//         return $iframe;
-//     }
-//
-//     this.generateURL = function(id) {
-//         let query = '?rel=0&showinfo=0&autoplay=1';
-//         return 'https://www.youtube.com/embed/' + id + query;
-//     }
-// }
-
-const FindVideos = function() {
+const FindVideos = function () {
     this.init();
 }
 
-FindVideos.prototype.init = function() {
+FindVideos.prototype.init = function () {
     let $videos = document.querySelectorAll('.view-block-wrap');
     for (let i in $videos) {
         if ($videos.hasOwnProperty(i)) {
@@ -112,13 +11,13 @@ FindVideos.prototype.init = function() {
     }
 }
 
-FindVideos.prototype.setupVideo = function($video) {
+FindVideos.prototype.setupVideo = function ($video) {
     let $link = $video.querySelector('.video__link');
     let $media = $video.querySelector('.video__media');
     let $button = $video.querySelector('.video__button');
     let id = this.parseMediaURL($media);
-    $video.addEventListener('click', () => {
-        let $iframe = this.createIframe(id);
+    $video.addEventListener('click', function () {
+        let $iframe = FindVideos.prototype.createIframe(id);
         $link.remove();
         $button.remove();
         $video.appendChild($iframe);
@@ -127,14 +26,14 @@ FindVideos.prototype.setupVideo = function($video) {
     $video.classList.add('video--enabled');
 }
 
-FindVideos.prototype.parseMediaURL = function($media) {
+FindVideos.prototype.parseMediaURL = function ($media) {
     let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
     let url = $media.src;
     let match = url.match(regexp);
     return match[1];
 }
 
-FindVideos.prototype.createIframe = function(id) {
+FindVideos.prototype.createIframe = function (id) {
     let $iframe = document.createElement('iframe');
     $iframe.setAttribute('allowfullscreen', '');
     $iframe.setAttribute('allow', 'autoplay');
@@ -143,7 +42,7 @@ FindVideos.prototype.createIframe = function(id) {
     return $iframe;
 }
 
-FindVideos.prototype.generateURL = function(id) {
+FindVideos.prototype.generateURL = function (id) {
     let query = '?rel=0&showinfo=0&autoplay=1';
     return 'https://www.youtube.com/embed/' + id + query;
 }
